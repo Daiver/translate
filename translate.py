72b57b2cf82a2b0a6fac054bc445dce19a1c965a
Kirill Klimov <ra22341@ya.ru>
2012-08-21 17:45:45 +0400

    clear code

--- translate.py
+++ translate.py
@@ -53,8 +53,6 @@ def main():
 
   """
 
-  #print 'DDE (C) 2011'
-
   url = "http://translate.google.com/translate_a/t?%s"
 
   list_of_params = {'client' : 't', 
@@ -64,22 +62,19 @@ def main():
                     'multires' : '1', }   
 
   #if len(sys.argv) >1:
-  if len(sys.argv)<5:
+  if len(sys.argv)<4:
       print (main.__doc__)
       return 0
-  if len(sys.argv)>=5:
+  if len(sys.argv)>=4:
     txt='';
     for x in range(4,len(sys.argv)):
       txt+=str(sys.argv[x]) +' '
-    #print (txt)
-    #return
     
     sourcelang=sys.argv[2]
     if sourcelang=='ru':
       try:
         buff=pytils.translit.detranslify(txt.encode('utf8'))
-        #print buff
-        #return
+        
       except:
         pass#print 'lib exception'
       else:
@@ -91,40 +86,26 @@ def main():
 
                            'tl' : sys.argv[3] })
 
-
   request = urllib2.Request(url % urllib.urlencode(list_of_params), 
 
     headers={ 'User-Agent': 'Mozilla/5.0', 'Accept-Charset': 'utf-8' })
 
   res = urllib2.urlopen(request).read()
 
-
-
   fixed_json = re.sub(r',{2,}', ',', res).replace(',]', ']')  
 
   data = json.loads(fixed_json)
 
-    
-
-  #simple translation
   for l in data[0]:
     print( "%s / %s / %s" % (l[0], l[1], 
 
               l[2] or l[3]))
-
-    
-
-  #abbreviation
 
   if not isinstance(data[1], basestring):
 
       print (data[1][0][0])
 
       print_params(data[1][0][1])
-
-      
-
-  #interjection  
 
   try:
 
@@ -137,12 +118,7 @@ def main():
   except Exception:
 
       print ("no interjection" )
-
-  #else:
-
-   # print (main.__doc__)
-
-    
+   
 
 if __name__ == '__main__':
 
